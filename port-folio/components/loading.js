9e02026a00d06f9e731f809f442f832fa30abcbc
emsiti2424 <sebastian.doana@softtekprojects.com>
2023-10-18 10:57:48 +0200

    updates

--- port-folio/components/loading.js
+++ port-folio/components/loading.js
@@ -1,13 +1,14 @@
 import { LitElement, html, css } from "lit";
 import styles from "../assets/styles/loading.scss";
+import { gsap } from "gsap";
 export class LoadingPage extends LitElement {
   static styles = css([styles]); // Use the css tag function from lit
   render() {
     return html`
       <div class="loading">
-        <div class="loading__box">*
+        <div class="loading__box">
           <div class="loading__text">
-            <div class="loading__text--border"></div>
+            <div class="loading__text--border loading__width_animation"></div>
             L
             <div class="loading__text--dot"></div>
             OADING EXPERIENCE
@@ -19,11 +20,34 @@ export class LoadingPage extends LitElement {
           <div class="loading__counter">
             <span>0%</span>
             <div class="loading__counter--number">100%</div>
-
           </div>
         </div>
       </div>
     `;
+  }
+  firstUpdated() {
+    this.__doLoading();
+  }
+  __doLoading() {
+    const bar = this.shadowRoot.querySelector(".loading__bar--inner");
+    const conter_num = this.shadowRoot.querySelector(
+      ".loading__counter--number"
+    );
+    const loading_bar = this.shadowRoot.querySelector(".loading__bar");
+    let c = 0;
+    let barInterval = setInterval(() => {
+      bar.style.width = c + "%";
+      conter_num.innerText = c + "%";
+      c++;
+      if (c === 101) {
+        clearInterval(barInterval);
+        gsap.to(loading_bar, {
+          duation: 5,
+          rotate: "90deg",
+          left: "1000%",
+        });
+      }
+    }, 20);
   }
 }
 
